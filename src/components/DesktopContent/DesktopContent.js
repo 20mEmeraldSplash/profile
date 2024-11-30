2d3c3235fa7a8e12a1a90e1e61197d460d9e61db
20mEmeraldSplash <yichenwaa@gmail.com>
2024-11-30 14:34:00 -0500

    (11-30-2024): Add resumr

--- src/components/DesktopContent/DesktopContent.js
+++ src/components/DesktopContent/DesktopContent.js
@@ -5,12 +5,24 @@ import FolderIcon from '../FolderIcon/FolderIcon'
 import pdfIcon from '../../assets/icons/pdf-icon.png'
 import folderIcon from '../../assets/icons/folder-icon.png'
 
+import resumePdf from '../../assets/pdf/resume.pdf'
+
 import './DesktopContent.css' // 引入 CSS 文件
 
 function DesktopContent({ children }) {
+  const openPdf = () => {
+    // 替换为你的 PDF 文件的路径
+    const newTab = window.open(resumePdf, '_blank') // 在新标签页打开 PDF 文件
+    if (!newTab) {
+      alert('Popup blocked! Please allow popups for this site.')
+    }
+  }
+
   return (
     <div className="desktop-content-container">
-      <FolderIcon imageSrc={pdfIcon} label="Resume" />
+      <div onClick={openPdf} style={{ cursor: 'pointer' }}>
+        <FolderIcon imageSrc={pdfIcon} label="Resume" />
+      </div>
       <FolderIcon imageSrc={folderIcon} label="Documents" />
     </div>
   )
